130546bb2c15519486ff314283ca52fde87e6503
robertodonoso <robertodonosopalma@outlook.com>
2023-12-12 20:49:22 -0300

    se agrega login al proyecto

--- src/redux/userSlice.ts
+++ src/redux/userSlice.ts
@@ -1,10 +1,37 @@
-import { combineReducers } from 'redux';
-import authReducer from './action/AutentificacionReducer';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
-const rootReducer = combineReducers({
-    auth: authReducer
-    // otros reducers van aquí
+export interface UserState {
+    username: string;
+    email: string;
+    isAuth: boolean;
+    role: string[]
+}
+
+const initialState: UserState = {
+    username: '',
+    email: '',
+    isAuth: false,
+    role: []
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        login: (_state, action: PayloadAction<UserState>) => {
+            const newState = { ...action.payload }
+            console.log("login reducer", newState)
+            return newState;
+        },
+        logout: () => {
+            console.log("logout reducer")
+            localStorage.removeItem('token');
+            return { ...initialState }
+
+        }
+    }
+
 });
 
-export default rootReducer;
-
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
