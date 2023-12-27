233d51429aaf59eec8c101553190453538a726e2
Joao Amestica <joao.amestica@gmail.com>
2023-12-27 02:38:08 -0300

    revision-home

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import Apps from './Apss.tsx'
-import { store } from './Redux/store.ts'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import Apps from "./Apss.tsx";
+import { store } from "./Redux/store.ts";
 
-
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-
-  <React.StrictMode>
-    <Provider store={store}>
-      
-        <BrowserRouter>
-          <Apps />
-        </BrowserRouter>
-      
-    </Provider>
-
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Apps />
+    </BrowserRouter>
+  </Provider>
+);
